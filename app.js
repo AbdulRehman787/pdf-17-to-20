07b12fc8200f69662c718856daa91cd80f001d2d
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-10-12 05:48:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//pdf assement 17 to 20
+
+
+// for (var i=0; i<=3 ;i++){
+// console.log(i)    
+// }
+
+// var arr = [0,1,3,4]
+// var arr1 = [1,0,1,2]
+// var arr2 = [2,1,0,1]
+// document.write(arr + "<br/>"+arr1+ "<br/>" + arr2)
+
+// for(var i = 0;i<=10;i++){
+//     document.write(i+"<br/>")
+// }
+
+// var inp = +prompt("Enter the number ");
+// for(var i=1;i<=10;i++){
+//     document.write(inp + " *" + i +"=" +inp* i+"<br/>")
+// }
+
+// var arr =["apple","banana","mango","Grapes"]
+// document.write(arr + "<br/>")
+
+// var countingNumber = [0,1,2,3,4,5,6,7,8,9,10]
+// document.write(countingNumber + "<br/>");
+// var reverseNumber = [10,9,8,7,6,5,4,3,2,1,0]
+// document.write(reverseNumber  + "<br/>")
+// var evenNumber = [2,4,6,8,10,12,14,16,18,20]
+// document.write(evenNumber  + "<br/>")
+// var oddNumber = [1,3,5,7,9,11,13,15,17,19]
+// document.write(oddNumber )
+
+// var inp = prompt("Enter your favorite cake" , "Choclate cake")
+// if(inp == "Choclate cake"){
+//     alert("It is your conform order")
+// }
+// else{
+//     alert("Sorry ! It is not here")
+// }
+
+
+//pdf assement 14 to 16  
+
+// var arr = []
+// console.log(arr)
+
+// var arr = ["Abdul","Rehman"]
+// console.log(arr)
+
+// var arr = [1,2,3,4,5];
+// console.log(arr)
+
+// var mixarr = [1,2,3,4,56,7,8 ,"Abdul ","Rehman",9,9]
+// console.log(mixarr)
+
